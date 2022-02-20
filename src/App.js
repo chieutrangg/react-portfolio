8d4e0477f69a724dec90ecb998781d0b9d605728
chieutrangg <chieutrangg@gmail.com>
2022-02-20 22:59:20 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import GlobalStyle from './globalStyles'
+import '../node_modules/normalize.css'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <GlobalStyle />
+      </div>
   )
 }
 
