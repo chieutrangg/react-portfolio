1dfa4e235df0e2d532ef735e58edbc443b0edf84
chieutrangg <chieutrangg@gmail.com>
2022-02-21 14:43:06 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import '../node_modules/normalize.css/'
 import { ThemeProvider } from 'styled-components'
 import { lightTheme } from './components/Themes'
 import { Switch, Route } from 'react-router-dom'
+// Components
 import Main from "./components/Main"
+import AboutPage from "./components/AboutPage"
+import BlogPage from "./components/BlogPage"
+import WorkPage from "./components/WorkPage"
+import MySkillsPage from "./components/MySkillsPage"
+
 
 const App = () => {
   return (
@@ -13,7 +19,10 @@ const App = () => {
     <ThemeProvider theme={lightTheme}>
       <Switch>
         <Route exact path="/" component ={Main}/>
-
+        <Route exact path="/about" component={AboutPage}/>
+        <Route exact path="/blog" component={BlogPage}/>
+       <Route exact path="/work" component={WorkPage}/>
+        <Route exact path="/skills" component={MySkillsPage}/>
       </Switch>
     </ThemeProvider>
     </div>
