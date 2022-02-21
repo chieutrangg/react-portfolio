b8d5d6a83aa9f7163f0ced76b65a5f45ace03c6d
chieutrangg <chieutrangg@gmail.com>
2022-02-21 19:49:47 +0200

    about page

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -27,7 +27,7 @@ const AboutPage = () => {
         <Main>
         I'm a front-end developer located in Finland. I love to create simple yet beautiful websites with great user experience.
         <br /> <br/>
-        I'm interested in the whole frontend stack Like trying new things and building great projects. I'm an independent freelancer and blogger. I love to write blogs and read books.
+        Mostly I am passionate about web coding, and I am a full-time learner Like trying new things and building projects. I'm an independent freelancer and blogger. I love to write blogs and read books.
         <br/> <br/>
         I believe everything is an Art when you put your consciousness in it. You can connect with me via social links.
         </Main>
