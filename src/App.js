24fe2744fb5b1c2efbf9e860b734607b45754c74
chieutrangg <chieutrangg@gmail.com>
2022-02-21 14:36:37 +0200

    react route dome added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import GlobalStyle from './globalStyles'
-import '../node_modules/normalize.css'
+import '../node_modules/normalize.css/'
+import { ThemeProvider } from 'styled-components'
+import { lightTheme } from './components/Themes'
+import { Switch, Route } from 'react-router-dom'
+import Main from "./components/Main"
 
 const App = () => {
   return (
     <div>
-      <GlobalStyle />
+    <GlobalStyle />
+    <ThemeProvider theme={lightTheme}>
+      <Switch>
+        <Route exact path="/" component ={Main}/>
+
+      </Switch>
+    </ThemeProvider>
     </div>
   )
 }
