e497051be8a69366ce25b7c101742aea0bdd1168
chieutrangg <chieutrangg@gmail.com>
2022-02-22 16:28:46 +0200

    updated blog

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -3,12 +3,12 @@
 export const Blogs = [
   {
     id: 1,
-    name: 'Build Website with ReactJS, Styled-components and GSAP for Scrolling Animations',
-    tags: ['react', 'gsap', 'styled-components'],
-    date: '13 May, 2021',
+    name: 'The "weird" Javascript',
+    tags: ['JavaScript'],
+    date: '27 Oct, 2021',
     imgSrc:
-      'https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png',
-    link: 'https://dev.to/codebucks/build-website-with-reactjs-styled-components-and-gsap-for-scrolling-animations-2f10'
+      'https://res.cloudinary.com/dpen0jyga/image/upload/v1645540004/portfolio/yuoOO7E_oymmob.png',
+    link: 'https://chieutrangg.wixsite.com/ticktocktechtalk/post/the-weird-javascript'
   },
   {
     id: 2,
