12297da85454bca0df5f1a6d4da97fcea883e006
chieutrangg <chieutrangg@gmail.com>
2022-02-22 17:23:35 +0200

    updated blog

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -12,12 +12,12 @@ export const Blogs = [
   },
   {
     id: 2,
-    name: 'How to create React Hamburger Menu using Styled-Components',
+    name: 'What is typescript? and why should use typescript?',
     tags: ['react', 'styled-components'],
     date: '5 July, 2021',
     imgSrc:
-      'https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625479747640%2F7KzwP9nmj.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75',
-    link: 'https://codebucks.hashnode.dev/react-hamburger-menu'
+      'https://res.cloudinary.com/dpen0jyga/image/upload/v1645543081/d05a5f_f42232fb746f400c82e5721fdbba5e2d_mv2_nfyfb8.png',
+    link: 'https://chieutrangg.wixsite.com/ticktocktechtalk/post/what-is-typescript-and-why-should-use-typescript'
   },
   {
     id: 3,
